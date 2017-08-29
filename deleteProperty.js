852832281fe52e4a8f62fb35b0aa891cd0c07fd6
James Wright <james@jamesswright.co.uk>
2017-08-29 16:20:59 +0100

    Initial commit. deleteProperty trap example

---
+++ deleteProperty.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const protectObject = (obj, blacklist) => new Proxy(obj, {
+    deleteProperty(target, prop) {
+        if (blacklist.includes(prop)) {
+            throw new Error(`Unable to delete property ${prop} from sealed object`);
+        }
+
+        return target[prop];
+    }
+});
+
+const person = {
+    name: 'Bob',
+    age: 16,
+};
+
+const blacklist = ['name'];
+
+const sealedPerson = protectObject(person, blacklist);
+delete sealedPerson.age;
+delete sealedPerson.name;
