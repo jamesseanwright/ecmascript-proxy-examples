af2e3ba781a299c30510aacd2ae87a4f85f83cf4
James Wright <james@jamesswright.co.uk>
2017-09-02 12:33:47 +0100

    Update fake DB import. Set encoding to UTF-8

--- examples/preventXss/index.js
+++ examples/preventXss/index.js
@@ -5,9 +5,9 @@ const url = require('url');
 const striptags = require('striptags');
 const readBody = require('./readBody');
 const connectToDb = require('./db'); // pretend this is a third-party database lib we can't modify...
-const db = connectToDb();
+const { get, set: unprotectedSet } = connectToDb();
 
-const protectedSet = new Proxy(db.set, {
+const set = new Proxy(unprotectedSet, {
     apply(target, thisArg, argumentsList) {
         const [ key, value ] = argumentsList;
         const escapedValue = striptags(value);
@@ -22,16 +22,16 @@ const HOME_KEY = 'home';
 const routes = new Map([
     ['GET', new Map([
         ['/', (req, res) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(db.get(HOME_KEY));
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(get(HOME_KEY));
         }],
     ])],
 
     ['POST', new Map([
         ['/update-home-content', (req, res) => readBody(req)
-            .then(body => protectedSet(HOME_KEY, body))
+            .then(body => set(HOME_KEY, body))
             .then(() => {
-                res.writeHead(200);
+                res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
                 res.end('OK');
             })
         ],
@@ -44,7 +44,7 @@ const server = http.createServer((req, res) => {
 
     if (!routesForMethod.has(pathname)) {
         res.writeHead(404);
-        res.end('Not found');
+        res.end('Not found', { 'Content-Type': 'text/plain; charset=utf-8' });
         return;
     }
 
