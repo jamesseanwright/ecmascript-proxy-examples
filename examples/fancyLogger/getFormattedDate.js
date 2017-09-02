bad3d79572a01a3d3c987aed0a425842a96becd4
James Wright <james@jamesswright.co.uk>
2017-09-02 17:06:47 +0100

    Fancy console example

---
+++ examples/fancyLogger/getFormattedDate.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { Intl } = global;
+
+const formatter = new Intl.DateTimeFormat('en-GB', {
+    hour12: false,
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+});
+
+const getFormattedDate = () => formatter.format(new Date());
+
+module.exports = getFormattedDate;
---
+++ examples/fancyLogger/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const getFormattedDate = require('./getFormattedDate');
+
+const formattableMethods = [
+    'log',
+    'info',
+    'warn',
+    'error',
+];
+
+const methodColours = new Map([
+    ['warn', '\x1b[1;33m'],
+    ['error', '\x1b[1;31m'],
+    ['default', '\x1b[0m'],
+]);
+
+function format(method, values) {
+    const defaultColour = methodColours.get('default');
+    const colour = methodColours.has(method) ? methodColours.get(method) : defaultColour;
+
+    return `${colour}[${getFormattedDate()}] - ${values.join(' ')}${defaultColour}`;
+}
+
+const fancyConsole = new Proxy(console, {
+    get(target, prop) {
+        if (formattableMethods.includes(prop)) {
+            return (...values) => target[prop](format(prop, values));
+        }
+
+        return target[prop];
+    }
+});
+
+fancyConsole.log('Hello', 'JS', 'Roundabout!');
+fancyConsole.warn('Warning!');
+fancyConsole.error('Oh no!', 'Error!');
